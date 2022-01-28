a7705d10080f199e08532fbd936e23195ff18d38
AliAdl <ali.olekh@gmail.com>
2022-01-29 03:26:54 +0330

    Add project files.

---
+++ ExtLog/Log.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SadrTools.ExtensionMethods;
+using System.IO;
+
+namespace ExtLog
+{
+    public static class Log
+    {
+        public static void Loged(this Exception ex)
+
+        {
+            string date = DateTime.Now.ToPersianDate('_');
+            using (StreamWriter sw = new StreamWriter(@$"E:\BootCamp\PracticeAtHome\IOPractice\ExLog\LogFolder\{date}.txt"))
+            {
+                sw.WriteLine("xxxxx");
+                sw.WriteLine(ex.ToString());
+                sw.WriteLine("xxxxx");
+            }
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using ExtLog;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<string> se = new List<string>();
+                se.Add("Ali");
+                se.Add("Rasool");
+                se.Add("Khashayar");
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(se[i]);
+                }
+            }
+            catch (Exception e)
+            {
+
+                e.Loged();
+            }
+        }
+    }
+}
